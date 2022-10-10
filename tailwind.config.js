c9cd455e82d26213b03da52a46f07f26c761293f
W0jno <filip.wojno03@o2.pl>
2022-10-10 20:49:43 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./*.html"],
+	theme: {
+		extend: {},
+		colors: {
+			backgroundGradient1: "#E7B505",
+			backgroundGradient2: "#FC9330",
+			formBackgroundGradient1: "#FF824F",
+			formBackgroundGradient2: "#FFE248",
+			btncolor: "#fff",
+		},
+		borderRadius: {
+			large: "40px",
+		},
+	},
+	plugins: [],
+};
